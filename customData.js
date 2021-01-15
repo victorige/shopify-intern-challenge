ed3afe1129f939245f66c020a9e0c37b0eacba07
victorige <victemige.technologies@gmail.com>
2021-01-15 17:22:48 +0100

    Shopify commit2

---
+++ customData.js
@@ -0,0 +1,44 @@
+//dummy images data
+export default [
+        {
+            id: 1,
+            title: "Beach 2 Fun",
+            url: "/images/beach.jpg"
+        }, {
+            id: 2,
+            title: "forest Animal",
+            url: "/images/forest.jpg"
+        }, {
+            id: 3,
+            title: "River",
+            url: "/images/boat.jpg"
+        }, {
+            id: 4,
+            title: "Green Sky",
+            url: "/images/green-sky.jpg"
+        },{
+            id: 5,
+            title: "Mountain",
+            url: "/images/mountain.jpg"
+        }, {
+            id: 6,
+            title: "Surfer",
+            url: "/images/surfer.jpg"
+        }, {
+            id: 7,
+            title: "Macbook air Working",
+            url: "/images/macbook.jpg"
+        }, {
+            id: 8,
+            title: "Ocean",
+            url: "/images/ocean.jpg"
+        }, {
+            id: 9,
+            title: "Home",
+            url: "/images/home.jpg"
+        }, {
+            id: 10,
+            title: "Blue Mountain",
+            url: "/images/blue-mountain.jpg"
+        },
+    ];
